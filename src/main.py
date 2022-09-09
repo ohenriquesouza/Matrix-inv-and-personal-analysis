d3f8ee437fe66267a297332e36ef5280b96d601f
ohenriquesouza <henriquesouzafagundes2003@gmail.com>
2022-09-09 14:09:30 -0300

    python finalizado

--- src/main.py
+++ src/main.py
@@ -1,1 +1,44 @@
-print("hello world")
+import pandas as pd
+import numpy as np
+
+#file reading
+aux = pd.read_excel(r"planilha.xls")
+
+#matrix
+default_matrix = np.zeros((12 , 12), dtype = np.float64)
+
+#first column 'DATA' remove
+aux = aux.drop('DATA', axis=1)
+
+#data searching
+i = 0
+while(i+12 < 6199):
+
+    default_matrix[0:12, 0] = round(aux["ITSA4"][i:i+12], 2 )
+    default_matrix[0:12, 1] = round(aux["BBDC4"][i:i+12], 2 )
+    default_matrix[0:12, 2] = round(aux["USIM5"][i:i+12], 2 )
+    default_matrix[0:12, 3] = round(aux["BRFS3"][i:i+12], 2 )
+    default_matrix[0:12, 4] = round(aux["BVMF3"][i:i+12], 2 )
+    default_matrix[0:12, 5] = round(aux["CMIG4"][i:i+12], 2 )
+    default_matrix[0:12, 6] = round(aux["GGBR4"][i:i+12], 2 )
+    default_matrix[0:12, 7] = round(aux["BBAS3"][i:i+12], 2 )
+    default_matrix[0:12, 8] = round(aux["ITUB4"][i:i+12], 2 )
+    default_matrix[0:12, 9] = round(aux["PETR4"][i:i+12], 2 )
+    default_matrix[0:12, 10] = round(aux["VALE5"][i:i+12], 2 )
+    default_matrix[0:12, 11] = round(aux["ABEV3"][i:i+12], 2 )
+
+    #is possible to invert
+    if(np.linalg.det(default_matrix) != 0):
+
+        #numpy library inverse operation
+        matrix_inv = np.linalg.inv(default_matrix)
+
+        #user output
+        #print(default_matrix)
+        print(matrix_inv)
+        print("\n-----------------------------------------------------------------------------\n")
+    #is not possible to invert  
+    else:
+        print("This matrix cannot be inverted!")
+
+    i = i + 1
