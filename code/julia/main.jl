8344a9482370695af756a29c8dd2ac570bb9b658
ohenriquesouza <henriquesouzafagundes2003@gmail.com>
2022-09-17 17:09:12 -0300

    julia teste

---
+++ code/julia/main.jl
@@ -0,0 +1,27 @@
+pwd()
+
+f = open("planilha.csv")
+
+close(f)
+
+totallines = open("planilha.csv") do f 
+
+    linecounter = 0
+
+    for line in eachline(f)
+
+        linecounter += 1
+    end
+
+    (linecounter)
+
+end
+
+read(f)
+
+close(f)
+
+f = open("planilha.csv")
+
+lines = readlines(f)
+
