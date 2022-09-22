295b9dc9c7fe447ef6f584f1b5a4205e197c3819
Henrique Souza <87656464+ohenriquesouza@users.noreply.github.com>
2022-09-22 11:39:34 -0300

    talvez funcione

--- code/julia/main.jl
+++ code/julia/main.jl
@@ -1,26 +1,32 @@
-using Pkg
 
-Pkg.add("XLSX")
+|----------------|
+|   using Pkg    |---> Essa parte deve ser adicionada no TERMINAL
+|   Pkg.add("")  |
+|----------------|
 
 using XLSX
 
 XLSX.openxlsx("planilha.xlsx", enable_cache = false) do f
 
-    sheet = f["Sheet1"]
+    sheet = f["planilha"]
+    
+    StartingLine = "B"
+    i = 2
+    StartingColumn = "M"
+    j = 13
 
-    for r in XLSX.eachrow(sheet)
-
-        rn = XLSX.row_number(r)
-
-        v1 = r[1]
-
-        v2 = r[2]
-
-        v3 = r["B"]
-
-        v4 = r[4]
-
-        println("v1=$v1, v2=$v2, v3=$v3, v4=$v4 ")
+    for r in XLSX.eachrow(planilha)
+        
+        Line = StartingLine * string(i)
+        
+        Column = StartingColumn * string(j)
+        
+        myMatrix = sheet["$Line:$Column"]
+        
+        println(inv((Float.(myMatrix))))
+        
+        i = i+1
+        j = j+1
 
     end
 end
