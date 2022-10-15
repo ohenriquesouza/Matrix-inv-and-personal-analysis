83cdd233d1ed48395872865d240997d41baf428c
Henrique Souza <87656464+ohenriquesouza@users.noreply.github.com>
2022-10-14 22:16:43 -0300

    Update main.py

--- code/python/main.py
+++ code/python/main.py
@@ -15,7 +15,10 @@ aux = aux.drop('DATA', axis=1)
 
 #data searching
 i = 0
-while(i+12 < 6199):
+
+cont = 0
+
+while(i+12 < 31):
 
     default_matrix[0:12, 0] = round(aux["ITSA4"][i:i+12], 2 )
     default_matrix[0:12, 1] = round(aux["BBDC4"][i:i+12], 2 )
@@ -32,27 +35,36 @@ while(i+12 < 6199):
 
     isaNaN = np.isnan(default_matrix)
 
+    print(isaNaN)
+
     #NaN validation
-    if(isaNaN.any() != 1):
+    if(isaNaN.any() == 0):
+
+        determinante = np.linalg.det(default_matrix)
+
+        determinante = round(determinante, 15)
 
         #is possible to invert
-        if(np.linalg.det(default_matrix) != 0):
+        if(determinante != 0):
 
             #numpy library inverse operation
             matrix_inv = np.linalg.inv(default_matrix)
 
             #user output
-            print("\n-----------------------------------------------------------------------------\n")
-            print(default_matrix)
-            print("\n#############################################################################\n")
             print(matrix_inv)
-            print("\n-----------------------------------------------------------------------------\n")
+            print("\n")
         #is not possible to invert  
         else:
             print("This matrix cannot be inverted!")
+            cont = cont + 1
 
         i = i + 1
     else:
         print("Some value in your file is not a number")
-    
+
+        i = i + 1
+
+
+print(cont)
+
 print("%s" % (time.time() - starting))
